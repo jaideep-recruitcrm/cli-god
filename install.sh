c858114f1d79fbb42439ea36d34f8f2fc6f5332a
jaideep-recruitcrm <jaideep@recruitcrm.io>
2023-12-30 01:20:10 +0530

    CLI-GOD

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ echo ""
 echo "Cloing Repository Temporarily"
 git clone $REPOSITORY_URL $TEMPORARY_DIRECTORY
 
-echo ""
-echo "Downloading NeoVim"
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
+# echo ""
+# echo "Downloading NeoVim"
+# curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
 
-echo ""
-echo "Downloading TPM"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# echo ""
+# echo "Downloading TPM"
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 git clone $REPOSITORY_URL $TEMPORARY_DIRECTORY
 
@@ -27,13 +27,11 @@ if [ -f /etc/os-release ]; then
     sudo apt install -y tmux
     cp -r $TEMPORARY_DIRECTORY/nvim /home/ubuntu/.config/nvim
     cp -r $TEMPORARY_DIRECTORY/tmux ~/.config/tmux
-    rm -rf $TEMPORARY_DIRECTORY
   elif [ "$ID" = "amzn" ]; then
     sudo yum update
     sudo yum install -y tmux
     cp -r $TEMPORARY_DIRECTORY/nvim /home/ec2-user/.config/nvim
     cp -r $TEMPORARY_DIRECTORY/tmux /home/ec2-user/.config/tmux
-    rm -rf $TEMPORARY_DIRECTORY
   else
     echo "Unsupported OS: $ID"
     exit 1
@@ -42,21 +40,22 @@ else
   echo "Unable to detect OS"
   exit 1
 fi
+rm -rf $TEMPORARY_DIRECTORY
 
-echo ""
-echo "Extracting NeoVim to /opt/"
-sudo tar -xvzf nvim-linux64.tar.gz -C /opt/
-rm nvim-linux64.tar.gz
+# echo ""
+# echo "Extracting NeoVim to /opt/"
+# sudo tar -xvzf nvim-linux64.tar.gz -C /opt/
+# rm nvim-linux64.tar.gz
 
-echo ""
-echo "Renaming extracted folder"
-sudo mv /opt/nvim-linux64 $DESTINATION_DIRECTORY
+# echo ""
+# echo "Renaming extracted folder"
+# sudo mv /opt/nvim-linux64 $DESTINATION_DIRECTORY
 
-echo ""
-echo "Updating .bashrc"
-echo "export NVIM_HOME=$DESTINATION_DIRECTORY"
-echo 'export PATH="$PATH:$NVIM_HOME/bin"'
-source ~/.bashrc
+# echo ""
+# echo "Updating .bashrc"
+# echo "export NVIM_HOME=$DESTINATION_DIRECTORY"
+# echo 'export PATH="$PATH:$NVIM_HOME/bin"'
+# source ~/.bashrc
 
-echo ""
-echo "INSTALLATION COMPLETE"
+# echo ""
+# echo "INSTALLATION COMPLETE"
