8e38800dff489d326c6591da4e36b6a3abdbda64
jaideep-recruitcrm <jaideep@recruitcrm.io>
2023-12-30 01:32:16 +0530

    CLI-GOD

--- install.sh
+++ install.sh
@@ -4,61 +4,39 @@ DESTINATION_DIRECTORY="/opt/nvim"
 REPOSITORY_URL="https://github.com/jaideep-recruitcrm/cli-god.git"
 TEMPORARY_DIRECTORY=$(mktemp -d)
 
-echo ""
 echo "Cloing Repository Temporarily"
 git clone $REPOSITORY_URL $TEMPORARY_DIRECTORY
 
-mkdir /home/ubuntu/.config/nvim
-mkdir /home/ubuntu/.config/tmux
+# Ensure the .config directory and its subdirectories exist
+mkdir -p /home/ubuntu/.config/nvim
+mkdir -p /home/ubuntu/.config/tmux
 
-# echo ""
-# echo "Downloading NeoVim"
-# curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
+echo "Downloading NeoVim"
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
 
-# echo ""
-# echo "Downloading TPM"
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Downloading TPM"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-git clone $REPOSITORY_URL $TEMPORARY_DIRECTORY
-
-echo ""
 echo "Installing TMUX"
-if [ -f /etc/os-release ]; then
-  . /etc/os-release
-  if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
-    sudo apt update
-    sudo apt install -y tmux
-    cp -r $TEMPORARY_DIRECTORY/nvim /home/ubuntu/.config/nvim
-    cp -r $TEMPORARY_DIRECTORY/tmux ~/.config/tmux
-  elif [ "$ID" = "amzn" ]; then
-    sudo yum update
-    sudo yum install -y tmux
-    cp -r $TEMPORARY_DIRECTORY/nvim /home/ec2-user/.config/nvim
-    cp -r $TEMPORARY_DIRECTORY/tmux /home/ec2-user/.config/tmux
-  else
-    echo "Unsupported OS: $ID"
-    exit 1
-  fi
-else
-  echo "Unable to detect OS"
-  exit 1
-fi
+sudo apt update
+sudo apt install -y tmux
+
+# Copy the configuration files from the repository
+cp -r $TEMPORARY_DIRECTORY/nvim/* /home/ubuntu/.config/nvim
+cp -r $TEMPORARY_DIRECTORY/tmux/* /home/ubuntu/.config/tmux
+
+# Clean up the temporary directory
 rm -rf $TEMPORARY_DIRECTORY
 
-# echo ""
-# echo "Extracting NeoVim to /opt/"
-# sudo tar -xvzf nvim-linux64.tar.gz -C /opt/
-# rm nvim-linux64.tar.gz
+echo "Extracting NeoVim to /opt/"
+sudo tar -xvzf nvim-linux64.tar.gz -C /opt/
+rm nvim-linux64.tar.gz
 
-# echo ""
-# echo "Renaming extracted folder"
-# sudo mv /opt/nvim-linux64 $DESTINATION_DIRECTORY
+echo "Renaming extracted folder"
+sudo mv /opt/nvim-linux64 $DESTINATION_DIRECTORY
 
-# echo ""
-# echo "Updating .bashrc"
-# echo "export NVIM_HOME=$DESTINATION_DIRECTORY"
-# echo 'export PATH="$PATH:$NVIM_HOME/bin"'
-# source ~/.bashrc
+echo "Updating .bashrc"
+echo "export NVIM_HOME=$DESTINATION_DIRECTORY" >> ~/.bashrc
+echo 'export PATH="$PATH:$NVIM_HOME/bin"' >> ~/.bashrc
 
-# echo ""
-# echo "INSTALLATION COMPLETE"
+echo "INSTALLATION COMPLETE"
