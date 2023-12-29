08c5898e9dd73ba55044ba1f62a636ec3f2394bb
jaideep-recruitcrm <jaideep@recruitcrm.io>
2023-12-30 01:09:23 +0530

    CLI-GOD

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 DESTINATION_DIRECTORY="/opt/nvim"
+REPOSITORY_URL="https://github.com/jaideep-recruitcrm/cli-god.git"
+TEMPORARY_DIRECTORY=$(mktemp -d)
 
+echo ""
+echo "Cloing Repository Temporarily"
+cp -r $TEMPORARY_DIRECTORY/nvim ~/.config/nvim
+cp -r $TEMPORARY_DIRECTORY/tmux ~/.config/tmux
+rm -rf $TEMPORARY_DIRECTORY
+
+echo ""
 echo "Downloading NeoVim" 
 curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
 
+echo ""
 echo "Downloading TPM"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+git clone $REPOSITORY_URL $TEMPORARY_DIRECTORY
+
+echo ""
 echo "Installing TMUX"
 if [ -f /etc/os-release ]; then
   . /etc/os-release
@@ -26,25 +39,20 @@ else
   exit 1
 fi
 
+echo ""
 echo "Extracting NeoVim to /opt/"
 sudo tar -xvzf nvim-linux64.tar.gz -C /opt/
 rm nvim-linux64.tar.gz
 
+echo ""
 echo "Renaming extracted folder"
 sudo mv /opt/nvim-linux64 $DESTINATION_DIRECTORY
 
+echo ""
 echo "Updating .bashrc"
 echo "export NVIM_HOME=$DESTINATION_DIRECTORY"
 echo 'export PATH="$PATH:$NVIM_HOME/bin"'
 source ~/.bashrc
 
-REPOSITORY_URL="https://github.com/jaideep-recruitcrm/cli-god.git"
-TEMPORARY_DIRECTORY=$(mktemp -d)
-git clone $REPOSITORY_URL $TEMPORARY_DIRECTORY
-
-cp -r $TEMPORARY_DIRECTORY/nvim ~/.config/nvim
-cp -r $TEMPORARY_DIRECTORY/tmux ~/.config/tmux
-
-rm -rf $TEMPORARY_DIRECTORY
-
+echo ""
 echo "INSTALLATION COMPLETE"
